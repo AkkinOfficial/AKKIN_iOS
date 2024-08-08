b9fc25823879bdc2ce67c1c57eb922f2acd61320
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-08 11:19:41 +0900

    add: AkkinIcon image asset 추가

--- AKKIN/AKKIN/Extension/Literal/ImageLiteral.swift
+++ AKKIN/AKKIN/Extension/Literal/ImageLiteral.swift
@@ -8,17 +8,6 @@
 import UIKit
 
 public enum AkkinImage {
-    public static var akkinTitle: UIImage { .load(name: "AKKIN_Title") }
-    public static var akkinIcon1: UIImage { .load(name: "icon_1")}
-    public static var akkinIcon2: UIImage { .load(name: "icon_2")}
-    public static var akkinIcon3: UIImage { .load(name: "icon_3") }
-    public static var akkinIcon4: UIImage { .load(name: "icon_4") }
-    public static var akkinIcon5: UIImage { .load(name: "icon_5") }
-    public static var akkinImage1: UIImage { .load(name: "image_1") }
-    public static var akkinImage2: UIImage { .load(name: "image_2")}
-    public static var akkinImage3: UIImage { .load(name: "image_3")}
-    public static var akkinImage4: UIImage { .load(name: "image_4")}
-    public static var akkinImage5: UIImage { .load(name: "image_5")}
     public static var akkinlogin: UIImage { .load(name: "login")}
     public static var akkinOnboarding1: UIImage { .load(name: "onboarding1")}
     public static var akkinOnboarding2: UIImage { .load(name: "onboarding2")}
@@ -29,14 +18,18 @@ public enum AkkinImage {
 public enum AkkinButton {
     public static var addButton: UIImage { .load(name: "addButton") }
     public static var backButton: UIImage { .load(name: "backButton") }
-    public static var checkButton: UIImage { .load(name: "checkButton") }
-    public static var detailButton: UIImage { .load(name: "detailButton") }
-    public static var editButton: UIImage { .load(name: "editButton") }
-    public static var homeButton: UIImage { .load(name: "homeButton") }
-    public static var myPageButton: UIImage { .load(name: "myPageButton") }
-    public static var optionButton: UIImage { .load(name: "optionButton") }
-    public static var previousButton: UIImage { .load(name: "previousButton") }
     public static var skipButton: UIImage { .load(name: "skipButton") }
+}
+
+public enum AkkinIcon {
+    public static var home: UIImage { .load(name: "ic_home") }
+    public static var homeFilled: UIImage { .load(name: "ic_homeFilled") }
+    public static var habit: UIImage { .load(name: "ic_habit") }
+    public static var habitFilled: UIImage { .load(name: "ic_habitFilled") }
+    public static var calendar: UIImage { .load(name: "ic_calendar") }
+    public static var calendarFilled: UIImage { .load(name: "ic_calendarFilled") }
+    public static var my: UIImage { .load(name: "ic_my") }
+    public static var myFilled: UIImage { .load(name: "ic_myFilled") }
 }
 
 extension UIImage {
