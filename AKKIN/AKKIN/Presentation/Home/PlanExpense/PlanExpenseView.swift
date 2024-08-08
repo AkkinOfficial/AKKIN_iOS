dbf8453e480e6ddbfa5e082eebb1e2671209771b
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-08 10:49:47 +0900

    env: 파일 추가

--- AKKIN/AKKIN/Presentation/Home/PlanExpense/PlanExpenseView.swift
+++ AKKIN/AKKIN/Presentation/Home/PlanExpense/PlanExpenseView.swift
@@ -5,4 +5,23 @@
 //  Created by 박지윤 on 8/8/24.
 //
 
-import Foundation
+import UIKit
+
+final class PlanExpenseView: BaseView {
+
+    // MARK: UI Components
+
+    // MARK: Properties
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+    }
+
+    // MARK: Event
+}
