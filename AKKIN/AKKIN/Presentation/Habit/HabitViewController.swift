08f1bf4ad7214d878f5bac079edeb040faaabdba
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-15 17:35:36 +0900

    feat: HabitView UI 구현

--- AKKIN/AKKIN/Presentation/Habit/HabitViewController.swift
+++ AKKIN/AKKIN/Presentation/Habit/HabitViewController.swift
@@ -10,17 +10,39 @@ import UIKit
 final class HabitViewController: BaseViewController {
 
     // MARK: UI Components
+    private let habitView = HabitView()
 
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationItem()
     }
 
     // MARK: Configuration
     override func configureSubviews() {
+        view.addSubview(habitView)
+        habitView.backgroundColor = .akkinBG
     }
 
     // MARK: Layout
     override func makeConstraints() {
+        habitView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+
+    // MARK: Navigation Item
+    private func setNavigationItem() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        let defaultAppearance = UINavigationBarAppearance()
+        defaultAppearance.backgroundColor = .akkinBG
+
+        let scrollAppearance = UINavigationBarAppearance()
+        scrollAppearance.backgroundColor = .akkinBG
+        scrollAppearance.shadowColor = nil
+
+        navigationController?.navigationBar.standardAppearance = defaultAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = scrollAppearance
     }
 }
