8de7e04b37a782212b2592cdc6623b5c3a8bedc2
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-15 19:27:34 +0900

    add: UIButton+Extension

---
+++ AKKIN/AKKIN/Extension/UIButton+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIButton+Extension.swift
+//  AKKIN
+//
+//  Created by 박지윤 on 8/15/24.
+//
+
+import UIKit
+
+extension UIButton {
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
