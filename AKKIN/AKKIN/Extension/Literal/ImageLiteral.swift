95c6005a1ad0fab865dc90a52b8fc4d6c1083a1e
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-26 12:16:19 +0900

    add: closeButton image asset

--- AKKIN/AKKIN/Extension/Literal/ImageLiteral.swift
+++ AKKIN/AKKIN/Extension/Literal/ImageLiteral.swift
@@ -19,6 +19,7 @@ public enum AkkinImage {
 public enum AkkinButton {
     public static var addButton: UIImage { .load(name: "add_button") }
     public static var backButton: UIImage { .load(name: "back_button") }
+    public static var closeButton: UIImage { .load(name: "close_button") }
     public static var detailButton: UIImage { .load(name: "detail_button") }
     public static var nextButton: UIImage { .load(name: "next_button") }
     public static var previousButton: UIImage { .load(name: "previous_button") }
