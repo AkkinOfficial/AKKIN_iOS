7b90d5848eac60c1f3b14db92ebbc6b431f575ca
hyeonju <shj040201@naver.com>
2024-09-02 16:58:21 +0900

    feat: BaseUITextField 추가

---
+++ AKKIN/AKKIN/Extension/UITextField+Extension.swift
@@ -0,0 +1,41 @@
+//
+//  UITextField+Extension.swift
+//  AKKIN
+//
+//  Created by 성현주 on 9/2/24.
+//
+
+import UIKit
+
+extension UITextField {
+
+    /// 글자 시작위치 변경
+    func addLeftPadding(width: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+
+    /// border 설정
+    func setRoundBorder() {
+        layer.cornerRadius = 16
+    }
+
+    /// 좌측 이미지 추가
+    func addLeftImage(image: UIImage) {
+        let leftImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        leftImageView.image = image
+
+        // 패딩 값 추가
+        let padding: CGFloat = 10.0
+        let paddingLeftView = UIView(frame: CGRect(x: 0, y: 0, width: leftImageView.frame.width + padding*2 , height: leftImageView.frame.height ))
+        leftImageView.frame.origin = CGPoint(x: padding, y: 0)
+        leftImageView.contentMode = .scaleAspectFit
+
+        paddingLeftView.addSubview(leftImageView)
+        self.leftView = paddingLeftView
+        self.leftViewMode = .always
+
+    }
+
+}
---
+++ AKKIN/AKKIN/Presentation/Base/BaseUITextField.swift
@@ -0,0 +1,29 @@
+//
+//  BaseUITextField.swift
+//  AKKIN
+//
+//  Created by 성현주 on 9/2/24.
+//
+
+import UIKit
+
+final class BaseUITextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupUI() {
+        self.backgroundColor = .akkinTextFieldBackGround
+        self.addLeftPadding(width: 16)
+        self.setRoundBorder()
+        self.autocapitalizationType = .none
+        self.clearButtonMode = .always
+    }
+}
+
