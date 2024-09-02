6b1e3e14bed75ee9f334bc4783c004ccb27a4528
JJongW <shinjw4675@gmail.com>
2024-09-02 18:47:26 +0900

    feat: MakePiggyBankView UI 구현

--- AKKIN/AKKIN/Presentation/Habit/MakePiggyBank/MakePiggyBankView.swift
+++ AKKIN/AKKIN/Presentation/Habit/MakePiggyBank/MakePiggyBankView.swift
@@ -10,17 +10,47 @@ import UIKit
 final class MakePiggyBankView: BaseView {
 
     // MARK: UI Components
+    private let scrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+    }
+
+    private let piggyBankStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.distribution = .fill
+    }
+
+    private let piggyBankSettingLabel = UILabel().then {
+        $0.text = "목표 기한과\n저축 금액을 설정해주세요."
+        $0.textColor = .black
+        $0.numberOfLines = 0
+        $0.lineBreakMode = .byWordWrapping
+        $0.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+    }
 
     // MARK: Properties
 
     // MARK: Configuration
     override func configureSubviews() {
         super.configureSubviews()
+
+        addSubview(scrollView)
+
+        scrollView.addSubview(piggyBankStackView)
+        piggyBankStackView.addSubview(piggyBankSettingLabel)
     }
 
     // MARK: Layout
     override func makeConstraints() {
         super.makeConstraints()
+
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        piggyBankSettingLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(32)
+            $0.leading.equalToSuperview().inset(24)
+        }
     }
 
     // MARK: Event
