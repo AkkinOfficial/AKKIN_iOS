87ca8877a230ba0246fa22c0f19e1042933e9493
hyeonju <shj040201@naver.com>
2024-09-02 21:57:42 +0900

    feat: BaseTextField padding 조절

--- AKKIN/AKKIN/Presentation/Base/BaseButton.swift
+++ AKKIN/AKKIN/Presentation/Base/BaseButton.swift
@@ -8,25 +8,49 @@
 import UIKit
 
 class BaseButton: UIButton {
-    
+
+    override var isEnabled: Bool {
+        didSet {
+            isEnabled ? setEnableButton() : setDisableButton()
+        }
+    }
+
     // MARK: Properties
     var tap: (() -> Void)?
-    
+
     // MARK: Initializer
     init() {
         super.init(frame: .zero)
         configureTap()
+        setBaseButton()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     // MARK: Configuration
     private func configureTap() {
         addTarget(self, action: #selector(handleEvent), for: .touchUpInside)
     }
-    
+
+    func setBaseButton() {
+        self.setTitle("", for: .normal)
+        self.backgroundColor = .akkinGreen
+        self.layer.cornerRadius = 16
+        self.isEnabled = false
+    }
+
+    func setEnableButton() {
+        isUserInteractionEnabled = true
+        backgroundColor = .akkinGreen
+    }
+
+    func setDisableButton() {
+        isUserInteractionEnabled = false
+        backgroundColor = .akkinGreen.withAlphaComponent(0.6)
+    }
+
     // MARK: Event
     @objc private func handleEvent() {
         tap?()
