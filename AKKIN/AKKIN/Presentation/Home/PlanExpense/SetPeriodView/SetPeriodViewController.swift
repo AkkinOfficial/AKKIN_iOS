94b7328125cb0237aad1f9bc4d0f9235e259bb37
hyeonju <shj040201@naver.com>
2024-09-08 02:12:03 +0900

    feat: calendar delegate 추가

--- AKKIN/AKKIN/Presentation/Home/PlanExpense/SetPeriodView/SetPeriodViewController.swift
+++ AKKIN/AKKIN/Presentation/Home/PlanExpense/SetPeriodView/SetPeriodViewController.swift
@@ -16,11 +16,13 @@ final class SetPeriodViewController: BaseViewController {
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setPeriodView.delegate = self
     }
 
     // MARK: Configuration
     override func configureSubviews() {
         view.addSubview(setPeriodView)
+        updateView()
     }
 
     // MARK: Layout
@@ -29,5 +31,34 @@ final class SetPeriodViewController: BaseViewController {
             $0.edges.equalToSuperview()
         }
     }
+
+    func updateView() {
+        setPeriodView.calendarView.onDateSelected = { [weak self] selectedDates in
+            guard let self = self else { return }
+
+
+            if selectedDates.count == 1 {
+                self.didUpdateDates(startDate: selectedDates.first, endDate: nil)
+            }else{
+                self.didUpdateDates(startDate: selectedDates.first, endDate: selectedDates.last)
+                setPeriodView.confirmButton.isEnabled = true
+            }
+        }
+    }
+
 }
 
+extension SetPeriodViewController: SetPeriodViewDelegate {
+    func didUpdateDates(startDate: Date?, endDate: Date?) {
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "M월 d일"
+
+        let startDateString = startDate != nil ? dateFormatter.string(from: startDate!) : ""
+        let endDateString = endDate != nil ? dateFormatter.string(from: endDate!) : ""
+
+        print("Start Date: \(startDateString), End Date: \(endDateString)")
+        setPeriodView.startDateLabel.text = startDateString
+        setPeriodView.endDateLabel.text = endDateString
+    }
+}
