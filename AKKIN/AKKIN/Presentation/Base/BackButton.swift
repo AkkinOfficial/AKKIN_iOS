df2107bb1eafac8a0d7f7013fe2189abf9e13586
JJongW <shinjw4675@gmail.com>
2024-09-14 23:18:30 +0900

    feat: BackButton 수정

--- AKKIN/AKKIN/Presentation/Base/BackButton.swift
+++ AKKIN/AKKIN/Presentation/Base/BackButton.swift
@@ -5,4 +5,37 @@
 //  Created by 신종원 on 9/14/24.
 //
 
-import Foundation
+import UIKit
+
+class BackButton: UIButton {
+
+    // MARK: Properties
+    var tapBack: (() -> Void)?
+
+    // MARK: Initializer
+    init() {
+        super.init(frame: .zero)
+        configureTap()
+        setBackButton()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Configuration
+    private func configureTap() {
+        addTarget(self, action: #selector(handleEvent), for: .touchUpInside)
+    }
+
+    func setBackButton() {
+        self.isEnabled = true
+        self.setImage(AkkinButton.backButton, for: .normal)
+        self.isUserInteractionEnabled = true
+    }
+
+    // MARK: Event
+    @objc private func handleEvent() {
+        tapBack?()
+    }
+}
