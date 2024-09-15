f975c040f6e240916b89f41e3cfb7530241603e9
hyeonju <shj040201@naver.com>
2024-09-15 21:55:04 +0900

    feat: customKeyBoard 추가

---
+++ AKKIN/AKKIN/Presentation/Home/EmptyHomeView.swift
@@ -0,0 +1,28 @@
+//
+//  EmptyHomeView.swift
+//  AKKIN
+//
+//  Created by 성현주 on 9/14/24.
+//
+
+import UIKit
+
+final class EmptyHomeView: BaseView {
+
+    // MARK: UI Components
+
+    // MARK: Properties
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+    }
+
+    // MARK: Event
+}
+
