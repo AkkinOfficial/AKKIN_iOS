c34a7bed79374ce583a4a8878eac872fa061f97a
hyeonju <shj040201@naver.com>
2024-09-17 22:18:16 +0900

    feat: emptyHomeView 추가

--- AKKIN/AKKIN/Presentation/Home/HomeViewController.swift
+++ AKKIN/AKKIN/Presentation/Home/HomeViewController.swift
@@ -2,7 +2,7 @@
 //  HomeViewController.swift
 //  AKKIN
 //
-//  Created by 박지윤 on 8/8/24.
+//  Created by 성현주 on 9/15/24.
 //
 
 import UIKit
@@ -10,32 +10,18 @@ import UIKit
 final class HomeViewController: BaseViewController {
 
     // MARK: UI Components
-    private let homeView = HomeView()
-
-    // MARK: Environment
-    private let router = BaseRouter()
 
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        router.viewController = self
     }
 
     // MARK: Configuration
     override func configureSubviews() {
-        view.addSubview(homeView)
-
-        homeView.tapExpense = { [weak self] in
-            guard let self else { return }
-            router.presentPlanExpenseViewController()
-        }
     }
 
     // MARK: Layout
     override func makeConstraints() {
-        homeView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
     }
 }
+
