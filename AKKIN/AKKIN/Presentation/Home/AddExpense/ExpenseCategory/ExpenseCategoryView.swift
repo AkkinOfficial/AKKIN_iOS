0bc41c0444d4a7d2799793f189ecb3dda49ee712
hyeonju <shj040201@naver.com>
2024-09-20 16:20:53 +0900

    feat: expensViewController 추가

--- AKKIN/AKKIN/Presentation/Home/AddExpense/ExpenseCategory/ExpenseCategoryView.swift
+++ AKKIN/AKKIN/Presentation/Home/AddExpense/ExpenseCategory/ExpenseCategoryView.swift
@@ -5,4 +5,27 @@
 //  Created by 성현주 on 9/20/24.
 //
 
-import Foundation
+import UIKit
+
+final class ExpenseCategoryView: BaseView {
+
+    // MARK: UI Components
+    private let scrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+    }
+
+    // MARK: Properties
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+    }
+
+    // MARK: Event
+}
+
