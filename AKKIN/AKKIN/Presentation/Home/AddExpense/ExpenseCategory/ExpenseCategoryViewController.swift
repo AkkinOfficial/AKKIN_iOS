cf5930fd67b9efd9e388c79552075ad3ec023967
hyeonju <shj040201@naver.com>
2024-09-20 16:20:53 +0900

    feat: expensViewController 추가

--- AKKIN/AKKIN/Presentation/Home/AddExpense/ExpenseCategory/ExpenseCategoryViewController.swift
+++ AKKIN/AKKIN/Presentation/Home/AddExpense/ExpenseCategory/ExpenseCategoryViewController.swift
@@ -5,4 +5,23 @@
 //  Created by 성현주 on 9/20/24.
 //
 
-import Foundation
+import UIKit
+
+final class ExpenseCategoryViewController: BaseViewController {
+
+    // MARK: UI Components
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+    }
+}
+
