ab157cfb695cfe17c0f1ffdf072c6b7bae36cc60
hyeonju <shj040201@naver.com>
2024-09-22 15:54:34 +0900

    feat: addExpenseView ui 구현

--- AKKIN/AKKIN/Presentation/Home/AddExpense/AddExpenseViewController.swift
+++ AKKIN/AKKIN/Presentation/Home/AddExpense/AddExpenseViewController.swift
@@ -10,17 +10,26 @@ import UIKit
 final class AddExpenseViewController: BaseViewController {
 
     // MARK: UI Components
+    private let addExpenseView = AddExpenseView()
+
+    // MARK: Environment
+    private let router = BaseRouter()
 
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        router.viewController = self
     }
 
     // MARK: Configuration
     override func configureSubviews() {
+       view.addSubview(addExpenseView)
     }
 
     // MARK: Layout
     override func makeConstraints() {
+        addExpenseView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
