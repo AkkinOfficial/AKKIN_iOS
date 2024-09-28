ccd119f487077b4aec52ad979f6e15457f1aa042
hyeonju <shj040201@naver.com>
2024-09-29 00:20:49 +0900

    feat: homeView 추가

---
+++ AKKIN/AKKIN/Presentation/Home/HomeModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeModel.swift
+//  AKKIN
+//
+//  Created by 성현주 on 9/29/24.
+//
+
+import Foundation
--- AKKIN/AKKIN/Presentation/Home/HomeView.swift
+++ AKKIN/AKKIN/Presentation/Home/HomeView.swift
@@ -10,19 +10,38 @@ import UIKit
 final class HomeView: BaseView {
 
     // MARK: UI Components
+    lazy var addExpenseButton: BaseButton = {
+        let button = BaseButton()
+        button.setTitle("지출 계획하기", for: .normal)
+        button.isEnabled = true
+        return button
+    }()
 
     // MARK: Properties
+    var tapAddExpense: (() -> Void)?
 
     // MARK: Configuration
     override func configureSubviews() {
         super.configureSubviews()
+        addSubview(addExpenseButton)
+
+        addExpenseButton.addTarget(self, action: #selector(didTapAddExpenseButton), for: .touchUpInside)
     }
 
     // MARK: Layout
     override func makeConstraints() {
         super.makeConstraints()
+
+        addExpenseButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalTo(safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(60)
+        }
     }
 
     // MARK: Event
+    @objc private func didTapAddExpenseButton() {
+        tapAddExpense?()
+    }
 }
 
