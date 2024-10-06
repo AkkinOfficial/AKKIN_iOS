82ce4c5dda1b0bb5d5841ca0f51507929565e192
JJongW <shinjw4675@gmail.com>
2024-10-06 16:31:17 +0900

    feat: piggyBankModel 추가

---
+++ AKKIN/AKKIN/Presentation/Habit/PiggyBankModel.swift
@@ -0,0 +1,13 @@
+//
+//  PiggyBankModel.swift
+//  AKKIN
+//
+//  Created by 신종원 on 10/5/24.
+//
+
+struct PiggyBankModel {
+    let emoji: String
+    let name: String
+    let memo: String
+    let savings: String
+}
