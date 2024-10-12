225602b52a37d00ac051902b2881cc41945615aa
Bibi-urssu <bibi.urssu@gmail.com>
2024-10-12 23:57:07 +0900

    network: Savings API 연결 - 3

--- AKKIN/AKKIN/Presentation/Base/BaseRouter.swift
+++ AKKIN/AKKIN/Presentation/Base/BaseRouter.swift
@@ -60,8 +60,8 @@ final class BaseRouter {
         viewController?.navigationController?.pushViewController(categoryDetailViewController, animated: true)
     }
 
-    func presentExpenseListViewController(month: Int, day: Int) {
-        let expenseListViewController = ExpenseListViewController(month: month, day: day)
+    func presentExpenseListViewController(date: String) {
+        let expenseListViewController = ExpenseListViewController(date: date)
         viewController?.navigationController?.pushViewController(expenseListViewController, animated: true)
     }
 
